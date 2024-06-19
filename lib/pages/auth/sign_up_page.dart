4105188aff070cd35851a3537930e55bdb5f588d
khany <khanyeinc@gmail.com>
2024-06-19 14:59:04 +0200

    Create a Dynamic Reusable Widget
    Convert a UI textfield to a dynamic reusable text field widget with Dyanamic Icon;Text;and Text Controller Fields

--- lib/pages/auth/sign_up_page.dart
+++ lib/pages/auth/sign_up_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/widgets/%20app_textfield.dart';
 
 import '../../utils/colors.dart';
 import '../../utils/dimensions.dart';
@@ -27,46 +28,10 @@ class SignUpPage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            margin: EdgeInsets.only(left: Dimensions.height20,right: Dimensions.height20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(Dimensions.radius30),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 10,
-                  spreadRadius: 7,
-                  offset: Offset(1, 10),
-                  color: Colors.grey.withOpacity(0.2)
-                )
-              ]
-            ),
-            child: TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  //hinttext,//prefixx icon,//focusedborder//enabledBorder//border
-                  hintText: "Email",
-                  prefixIcon: Icon(Icons.email, color: AppColors.yellowColor,),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(Dimensions.radius30),
-                    borderSide: const BorderSide(
-                      width: 1.0,
-                      color: Colors.white,
-                    )
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(Dimensions.radius30),
-                      borderSide: const BorderSide(
-                        width: 1.0,
-                        color: Colors.white,
-                      )
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(Dimensions.radius30),
-                  )
-                ),
-            ),
-          )
+          AppTextField(textController: emailController,
+              hintText: "Email",
+              icon: Icons.email)
+
         ],
       ),
     );
