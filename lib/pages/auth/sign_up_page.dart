3c48594ef06eb37c3c9b69bed470258e5a61bab1
khany <khanyeinc@gmail.com>
2024-07-10 18:02:09 +0200

    Account Page Complete
    Fix Sign Up and Account Page UI

--- lib/pages/auth/sign_up_page.dart
+++ lib/pages/auth/sign_up_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/base/show_snackbar.dart';
 import 'package:food_delivery/models/signup_model.dart';
+import 'package:food_delivery/routes/route_helper.dart';
 import 'package:food_delivery/widgets/%20app_textfield.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:get/get.dart';
@@ -56,6 +57,7 @@ class SignUpPage extends StatelessWidget {
         authController.registrationController(model).then((status){
           if(status.isSuccess){
             print("Registration Successful");
+            Get.offNamed(RouteHelper.getInitial());
           }else{
             showSnackBar(status.message);
           }
